bea9b8edce21df9a98aa215eb5575735eb6f547a
rashokanand <ashok.anand91@gmail.com>
2026-02-12 11:46:06 +0530

    Add createGrid and functionality

--- javascript.js
+++ javascript.js
@@ -5,7 +5,6 @@ for (let i = 0; i < 16*16; i++) {
     gridDivs.push(div);
 }
 
-const divClass = "background-color: brown;";
 
 //Append created divs to container
 // And add event listeners to each
@@ -20,8 +19,35 @@ gridDivs.forEach(
 )
 
 function createNewGrid(e) {
-    const gridCount = prompt("Enter a number for each side of the grid");
-    console.log(gridCount);
+    const gridCount = Number(prompt("Enter a number for each side of the grid. (Max 100)"));
+    // console.log(gridCount);
+    if (gridCount >= 100 || isNaN(gridCount)) {
+        alert("Your input is invalid. Please try again.");
+        return 0;
+    }
+    // recreate grid
+    gridDivs.forEach(
+        (div) => div.remove()
+    );
+    
+    gridDivs = [];
+    for (let i = 0; i < gridCount * gridCount; i++) {
+        const div = document.createElement("div");
+        gridDivs.push(div);
+    }
+
+    gridDivs.forEach(
+        (div) => {
+            div.style.height = `${480 / gridCount}px`;//Modify dimensions of new grid square
+            div.style.width = `${480 / gridCount}px`;
+            containerDiv.appendChild(div);
+            div.addEventListener("mouseenter", () => {
+                div.classList.add('hover');
+            })
+        }
+    )
+    // console.log(gridDivs);
+
 }
 
 newGridButton = document.querySelector("button");
