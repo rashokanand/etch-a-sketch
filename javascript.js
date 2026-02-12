d56f4937a5e7c17061d9e1ef8c84fde75b2211d5
rashokanand <ashok.anand91@gmail.com>
2026-02-12 09:33:00 +0530

    Add draw feature using hover

--- javascript.js
+++ javascript.js
@@ -5,10 +5,16 @@ for (let i = 0; i < 16*16; i++) {
     gridDivs.push(div);
 }
 
+const divClass = "background-color: brown;";
+
 //Append created divs to container
+// And add event listeners to each
 const containerDiv = document.querySelector(".container");
 gridDivs.forEach(
     (div) => {
         containerDiv.appendChild(div);
+        div.addEventListener("mouseenter", () => {
+            div.classList.add('hover');
+        })
     }
 )
